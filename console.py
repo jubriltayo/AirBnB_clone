4e41ac7996ec8dd2e6e0fe2e0dfca441316f3c02
Tayo Jubril <jubriltayo@gmail.com>
2023-02-11 13:27:29 +0100

    error fixed

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class for command interpreter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """end of file command to exit program
+        """
+        return True
+
+    def emptyline(self):
+        """empty line no longer print last command"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
