8e00373c17d1a0971c543d58000c16f5157fea9d
lawal toheeb damilare <lawaltd088@gmail.com>
2023-02-11 07:37:55 +0000

    console interpreter

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+
+    promt = "(hbnb)"
+
+    def do_quit(self, line):
+        """quit command to exit"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Empty line and enter shouldnt execute the last program"""
+        pass
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
