26345f524c79ea837de2c7b904c6bb886479523b
Tayo Jubril <jubriltayo@gmail.com>
2023-02-11 01:30:35 +0100

    tasks 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,22 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes class with instance attributes:
             id, created_at, updated_at
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+        else:    
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string of the instance"""
