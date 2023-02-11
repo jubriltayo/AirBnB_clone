e1480193ac2499f362292d3bf3fd187cbac29d81
lawal toheeb damilare <lawaltd088@gmail.com>
2023-02-11 15:57:29 +0000

    updated

--- console.py
+++ console.py
@@ -4,20 +4,22 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-    promt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, line):
         """quit command to exit"""
         return True
 
     def do_EOF(self, line):
-        """EOF command to exit the program"""
+        """EOF command to exit the program
+        """
         print()
         return True
 
     def emptyline(self):
-        """Empty line and enter shouldnt execute the last program"""
+        """Empty line and enter shouldnt execute the last program
+        """
         pass
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         HBNBCommand().cmdloop()
